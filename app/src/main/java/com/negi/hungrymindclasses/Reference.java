31e68fb48e52234147dcf6e3f60c53f3bfb93ffa
Arvind Negi <negi.arvind03@gmail.com>
2019-12-29 23:48:11 +0530

    get back to home

--- app/src/main/java/com/negi/hungrymindclasses/Reference.java
+++ app/src/main/java/com/negi/hungrymindclasses/Reference.java
@@ -3,6 +3,7 @@ package com.negi.hungrymindclasses;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -23,15 +24,28 @@ ProgressDialog pd;
         pd.setMessage("PLEASE WAIT LOADING");
         pd.show();
         webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
-        webView.loadUrl("http://hungrymindclasses.com/");
+        webView.loadUrl("http://hungrymindclasses.com/category/math/");
     }
     private class MyBrowser extends WebViewClient{
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
+
             if(!pd.isShowing())
                 pd.show();
             return true;
+        }
+
+        @Override
+        public void doUpdateVisitedHistory(WebView view, String url, boolean isReload) {
+            if(url.equals("http://hungrymindclasses.com/")) {
+                Intent intent=new Intent(Reference.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                pd.dismiss();
+                startActivity(intent);
+                finish();
+            }
+            super.doUpdateVisitedHistory(view, url, isReload);
         }
 
         @Override
