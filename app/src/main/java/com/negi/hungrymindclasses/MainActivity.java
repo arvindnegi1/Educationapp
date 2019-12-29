4b454fadcc3ca3c4f11357772fa616de30a30eae
Arvind Negi <negi.arvind03@gmail.com>
2019-12-29 13:04:24 +0530

    added floating link

--- app/src/main/java/com/negi/hungrymindclasses/MainActivity.java
+++ app/src/main/java/com/negi/hungrymindclasses/MainActivity.java
@@ -3,12 +3,38 @@ package com.negi.hungrymindclasses;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+FloatingActionButton fab,fabfb,fabyt;
+int show=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    fab=findViewById(R.id.link);
+    fabfb=findViewById(R.id.fb);
+    fabyt=findViewById(R.id.youtube);
+    fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==fab)
+        {
+            if(show==0)
+            {
+                fabfb.show();
+                fabyt.show();
+                show=1;
+            }
+            else{
+                fabfb.hide();
+                fabyt.hide();
+                show=0;
+            }
+        }
     }
 }
--- app/src/main/java/com/negi/hungrymindclasses/splashScreen.java
+++ app/src/main/java/com/negi/hungrymindclasses/splashScreen.java
@@ -2,6 +2,7 @@ package com.negi.hungrymindclasses;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
@@ -21,7 +22,7 @@ LinearLayout l2;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
+                startActivity(new Intent(splashScreen.this,MainActivity.class));
             }
         },3000);
 
